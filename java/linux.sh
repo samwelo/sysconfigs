0c89db3f051ebdcd48247fa6c735769eb4fa8f5e
Sam <samwelo@outlook.com>
2013-11-01 14:55:57 -0500

    Update linux.sh

--- java/linux.sh
+++ java/linux.sh
@@ -1,3 +1,3 @@
 #set java home
-#do this on the terminal
+#do this on the terminal or on .bashrc so that any time terminal is open this is setup
 export JAVA_HOME="C:/Program Files/Java/jdk1.7.0_45"
