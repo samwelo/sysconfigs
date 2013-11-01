7fafbaa6d1e1f8b7c1bd0a829b299fc4351508e8
Sam <samwelo@outlook.com>
2013-11-01 15:41:00 -0500

    Update linux.sh

--- java/linux.sh
+++ java/linux.sh
@@ -1,3 +1,11 @@
 #set java home
 #do this on the terminal or on .bashrc so that any time terminal is open this is setup
 export JAVA_HOME="C:/Program Files/Java/jdk1.7.0_45"
+
+#for Maven To Work
+export JAVA_HOME="C:/Program Files/Java/jdk1.7.0_45"
+
+export M2_HOME="C:\maven\apache-maven-3.1.1"
+export M2=$M2_HOME/bin
+export MAVEN_OPTS="-Xms256m -Xmx512m"
+export PATH=$M2:$PATH
